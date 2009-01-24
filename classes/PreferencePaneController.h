3eb1cfaf483369d80dd7a1d7e85fd52c11b69f12
Jelle Vandebeeck <jelle@fousa.be>
2009-01-24 17:03:27 +0000

    added the preference pane functionality

---
+++ classes/PreferencePaneController.h
@@ -0,0 +1,16 @@
+//
+//  PreferencePaneController.h
+//  LazyPoken
+//
+//  Created by Jelle Vandebeeck on 24/01/09.
+//  Copyright 2009 KBC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface PreferencePaneController : NSPreferencePane {
+
+}
+
+@end
---
+++ classes/PreferencePaneController.m
@@ -0,0 +1,13 @@
+//
+//  PreferencePaneController.m
+//  LazyPoken
+//
+//  Created by Jelle Vandebeeck on 24/01/09.
+//  Copyright 2009 KBC. All rights reserved.
+//
+
+#import "PreferencePaneController.h"
+
+@implementation PreferencePaneController
+
+@end
