312924a7f61068f2aeb13b74c29265168b598743
Jelle Vandebeeck <jelle@fousa.be>
2009-02-01 17:25:43 +0000

    added the wrapper

---
+++ classes/LPWrapperController.m
@@ -0,0 +1,57 @@
+/*
+ Copyright (c) 2009 Jelle Vandebeeck, Simon Schoeters
+ 
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+ 
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ 
+ Created by Jelle Vandebeeck on 2009.01.31.
+ */
+
+#import "LPWrapperController.h"
+
+@implementation LPWrapperController
+
+-(id)init
+{
+	self = [super init];
+	
+	NSTask *serviceTask = [[NSTask alloc] init];
+	
+	// Create the ResourcePath for the script to launch
+	NSMutableString *scriptPath = [NSMutableString stringWithString:[[[NSBundle mainBundle] bundlePath] stringByDeletingLastPathComponent]];
+	[scriptPath appendString:@"/"];
+	[scriptPath appendString:LPScriptName];
+	
+	// Launch the script and save the process id
+	[serviceTask setLaunchPath:scriptPath];
+	
+	NSMutableDictionary *environment = [NSMutableDictionary dictionaryWithDictionary:[serviceTask environment]];
+    [environment setObject:@"" forKey:LPBUndleTag];
+    [serviceTask setEnvironment:environment];
+	
+	[serviceTask launch];
+	
+	NSLog(@"Started the LazyPoken agent");
+	
+	return self;
+}
+
+@end
